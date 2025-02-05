a566abeaf427d00a625c2da78ff045531ffaac58
jeisaRaja <jeisa.raja.jrt@gmail.com>
2025-02-05 16:32:13 +0700

    feat: add collectable tree log
    - fixed y-sorting origin
    - added sprite for tree log

--- scenes/objects/tree_1.gd
+++ scenes/objects/tree_1.gd
@@ -1,15 +1,16 @@
 extends Sprite2D
 
+@export var log_scene: PackedScene
+
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
 
 func _ready():
-	hurt_component.on_hurt.connect(_on_hurt.rpc)
+	hurt_component.on_hurt.connect(_on_hurt)
 	damage_component.max_damaged_reached.connect(_on_max_damaged_reached)
 
 
-@rpc("any_peer", "call_local", "reliable")
 func _on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
 	material.set_shader_parameter("shake_intensity", 1)
@@ -17,13 +18,13 @@ func _on_hurt(hit_damage: int) -> void:
 	material.set_shader_parameter("shake_intensity", 0.0)
 
 
-# func shake_tree():
-# 	damage_component.apply_damage(hit_damage)
-# 	material.set_shader_parameter("shake_intensity", 1)
-# 	await get_tree().create_timer(0.5).timeout
-# 	material.set_shader_parameter("shake_intensity", 0.0)
-
-
 func _on_max_damaged_reached():
-	print("max damage reached")
+	add_log_scene.rpc.call_deferred()
 	queue_free()
+
+
+@rpc("any_peer", "call_local", "reliable")
+func add_log_scene() -> void:
+	var log_instance = log_scene.instantiate() as Node2D
+	log_instance.global_position = global_position
+	get_parent().add_child(log_instance)
